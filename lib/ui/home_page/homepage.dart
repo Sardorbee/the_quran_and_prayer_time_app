7e843ef4f85b2d10330a60ed34067363cb84fba7
Sardorbee <sardorgoyibnazarov@gmail.com>
2023-07-11 16:23:45 +0500

    adad

--- lib/ui/home_page/homepage.dart
+++ lib/ui/home_page/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Center(
+              child: SvgPicture.asset('assets/svg/splash_page/quran_book.svg',
+                  height: 200),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

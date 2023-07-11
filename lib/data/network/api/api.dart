92bf9094a657e1c96bc2b3d333d7a38bb56d26c1
Sardorbee <sardorgoyibnazarov@gmail.com>
2023-07-11 15:45:09 +0500

    added api

--- lib/data/network/api/api.dart
+++ lib/data/network/api/api.dart
@@ -1,3 +1,20 @@
+import 'package:the_quran_and_prayer_time_app/data/model/prayermodel.dart';
+import 'package:the_quran_and_prayer_time_app/data/network/api/universal.dart';
+import 'package:http/http.dart' as http;
+
 class Prayerprovider {
- 
+  Future<UniversalResponse> getalltimes() async {
+    Uri uri = Uri.parse("https://islomapi.uz/api/present/day?region=Toshkent");
+    try {
+      http.Response response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        // print(response.body);
+        return UniversalResponse(data: prayerModelFromJson(response.body));
+      }
+      return UniversalResponse(error: "ERROR");
+    } catch (error) {
+      return UniversalResponse(error: error.toString());
+    }
+  }
 }
